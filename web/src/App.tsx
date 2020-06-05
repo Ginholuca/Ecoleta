b7f3c33caf32915f8b88d418edae250444911158
Giovanni De Luca <ginholucacorrea@icloud.com>
2020-06-05 18:37:01 -0300

    web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header'
+// import Header from './Header'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
-    <Header />
+    <Home />
     </>
   );
 }
